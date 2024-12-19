78bc62ee6fb23b8d089d623afec13f1e70cfa3f2
Raul Castro Palmett <rcastrop@outlook.com>
2024-12-19 10:27:07 -0500

    `Update pyproject.toml: change project name and add test dependency`

---
+++ start-env.sh
@@ -0,0 +1,17 @@
+python3.10 -m venv .venv
+source .venv/bin/activate
+# pip install --upgrade pip --quiet
+# pip install --upgrade setuptools wheel --quiet
+# pip install -r requirements.txt --quiet
+
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+
+
+#deactivate
+
+#source ../.venv/bin/activate
+
+python -m pip install -e ".[complete,test]"
+
+pip list
